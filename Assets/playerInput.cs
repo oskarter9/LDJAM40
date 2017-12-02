6058c44f10b38ac2f353f473b31affbd20cd926b
EduardoSimon <al341513@uji.es>
2017-12-02 15:28:23 +0100

    Added Local Multi

---
+++ Assets/playerInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class playerInput : MonoBehaviour
+{
+
+	public float speedH;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float delta = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speedH;
+		transform.position = new Vector3(transform.position.x+delta,transform.position.y,transform.position.z);
+	}
+}
