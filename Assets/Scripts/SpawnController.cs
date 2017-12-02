64ced2ab88893bbe541e9f250a45dd61b4f81e18
Marc <al341516@uji.es>
2017-12-02 18:31:03 +0100

    Strips

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -4,13 +4,61 @@ using UnityEngine;
 
 public class SpawnController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Strip strip;
+
+    private GameObject[] spawns;
+
+    [SerializeField]
+    private int numSpawns;
+
+    [SerializeField]
+    private GameObject spawn;
+
+    private float distanceBetweenSpawns;
+    private int screenSize = 16;
+    
+    void Awake()
+    {
+        Debug.Assert(strip);
+    }
+
+    void Start()
+    {
+        spawns = new GameObject[numSpawns];
+        distanceBetweenSpawns = (float) screenSize / numSpawns;
+
+        InitSpawns();
+    }
+
+    void InitSpawns()
+    {
+        for(int i = 0; i<numSpawns; i++)
+        {
+            spawns[i] = Instantiate(spawn, new Vector3(-screenSize/2 + distanceBetweenSpawns/2 + distanceBetweenSpawns * i, 6, 0), Quaternion.identity);
+            strip.CreateBlueStrip(Random.Range(3,8), i, spawns[i]);
+        }
+    }
+
+    bool CheckActiveStrip(GameObject strip)
+    {
+        if (strip.transform.childCount > 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void MoveStrip(GameObject strip, float vel)
+    {
+        if(CheckActiveStrip(strip))
+        {
+            strip.transform.Translate(new Vector3(0, -vel*Time.deltaTime , 0));
+        }
+    }
+
+    void Update()
+    {
+    }
+
+
 }
--- Assets/Scripts/Strip.cs
+++ Assets/Scripts/Strip.cs
@@ -1,16 +1,47 @@
-﻿using System.Collections;
+ ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Strip : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	private string blueLetters = "BlueLetters";
+    private string redLetters = "RedLetters";
+
+    [SerializeField]
+	private Object[] blueObjects;
+    [SerializeField]
+    private Object[] redObjects;
+
+    private Transform[] children;
+
+    void Awake()
+    {
+        children = GetComponentsInChildren<Transform>();
+
+        blueObjects = Resources.LoadAll(blueLetters, typeof(GameObject));
+        redObjects = Resources.LoadAll(redLetters, typeof(GameObject));
+    }
+
+	void Start(){
+
+        
+    }
+
+    public Transform[] getChildren()
+    {
+        return children;
+
+
+    }
+
+    public void CreateBlueStrip(int size, int numOfStrips, GameObject gO)
+    {
+        for(int i = 0; i<size; i++)
+        {
+            Instantiate(blueObjects[0], new Vector3(gO.transform.position.x, gO.transform.position.y + i, gO.transform.position.z), Quaternion.identity, children[numOfStrips+1]);
+        }
+    }
+
+    
 }
