034b7643ba6fe5391f9bc942e3ddbb557ee76388
oskarter9 <oscar5teruel@gmail.com>
2017-12-03 21:43:51 +0100

    muchas particulas y nivel

---
+++ Assets/Scripts/sunDown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sunDown : MonoBehaviour {
+
+    public float numMinutes;
+    public float finalSunPos = -8.4f;
+    private float downRatio;
+	// Use this for initialization
+	void Start () {
+
+        downRatio = finalSunPos / (numMinutes * 60);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(transform.position.x, transform.position.y + downRatio * Time.deltaTime, transform.position.z);
+
+        if (transform.position.y <= finalSunPos)
+        {
+            downRatio = 0;
+            Debug.Log("ACABADO!");
+        }
+	}
+}
