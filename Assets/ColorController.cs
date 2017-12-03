e9dca92738b7219716c5f30565e937088d7f3626
Marc <al341516@uji.es>
2017-12-03 21:03:30 +0100

    GrowPlayer

---
+++ Assets/ColorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorController : MonoBehaviour {
+
+    public PlayerInput playerInput;
+    public PlayerColor playerColor { get; set; }
+
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+
+        if (playerInput.PlayerId1 == PlayerInput.PlayerId.Player1)
+        {
+            playerColor = PlayerColor.RedColor;
+        }
+
+        else
+        {
+            playerColor = PlayerColor.BlueColor;
+        }
+    }
+
+    public enum PlayerColor
+    {
+        RedColor,BlueColor 
+    }
+
+}
