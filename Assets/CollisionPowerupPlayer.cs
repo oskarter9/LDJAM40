0fe02b17eb95b963306910de3ca736d6886913a7
Ricardo <lacajadericky@blogspot.com>
2017-12-04 21:48:33 +0100

    seguridad

--- Assets/CollisionPowerupPlayer.cs
+++ Assets/CollisionPowerupPlayer.cs
@@ -16,26 +16,21 @@ public class CollisionPowerupPlayer : MonoBehaviour {
 		
 	}
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
-        {
-
-
-            Destroy(this.gameObject);
-            Debug.Log("destruido");
-            spawnController.PowerupFreezeIsDestroyed();
-
-        }
-        
-       
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Floor")
         {
 
             spawnController.PowerupIsGrounded();
+
+
+        }
+
+        if (collision.gameObject.tag == "Player1" ||collision.gameObject.tag == "Player2")
+        {
+            Destroy(this.gameObject);
+            Debug.Log("destruido");
+            spawnController.PowerupFreezeIsDestroyed();
 
 
         }
