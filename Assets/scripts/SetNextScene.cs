5cb9e9ae1d5581c5dd93efa0072416877f2cdc94
EduardoSimon <al341513@uji.es>
2017-12-04 16:15:13 +0100

    Scene Hierarchy

---
+++ Assets/scripts/SetNextScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetNextScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        GameManager.Instance.sceneLoader.nextSceneToLoadIndex++;
+    }
+	
+
+}
