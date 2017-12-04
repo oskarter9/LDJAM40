dcb25fdf59a003e715321da8c48068f28826d40b
Ricardo <lacajadericky@blogspot.com>
2017-12-04 21:02:05 +0100

    cambiadas cosas de las fisicas y añadido powerup

---
+++ Assets/CollisionPowerupPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionPowerupPlayer : MonoBehaviour {
+
+
+    private SpawnController spawnController;
+	// Use this for initialization
+	void Start () {
+        spawnController = GameObject.Find("SpawnController").GetComponent<SpawnController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
+        {
+
+
+            Destroy(this.gameObject);
+            Debug.Log("destruido");
+            spawnController.PowerupFreezeIsDestroyed();
+
+        }
+        
+       
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+
+            spawnController.PowerupIsGrounded();
+
+
+        }
+    }
+}
