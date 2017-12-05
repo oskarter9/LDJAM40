06b3c125d41f59d34301b395888bdd2b3f6d5c1f
EduardoSimon <al341513@uji.es>
2017-12-05 02:25:15 +0100

    Added Ambient Music

---
+++ Assets/scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour {
+
+    public List<Sound> sounds = new List<Sound>();
+
+	// Use this for initialization
+	void Awake () {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+	}
+    
+    public void Play(string name)
+    {
+        Sound s = sounds.Find(sound => sound.name == name);
+        s.source.Play();
+    }
+}
