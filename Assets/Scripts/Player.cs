2e9c125a71c642b45a0b4cccbc4650f33cf58cab
oskarter9 <oscar5teruel@gmail.com>
2017-12-05 02:05:36 +0100

    Ale, to en regla

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Controller2D))]
 
 public class Player : MonoBehaviour
 {
---
+++ Assets/Scripts/StartGameFromRules.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameFromRules : MonoBehaviour {
+
+    public void StartNextScene()
+    {
+        GameManager.Instance.sceneLoader.LoadSceneInOrder(GameManager.Instance.sceneLoader.nextSceneToLoadIndex);
+    }
+}
